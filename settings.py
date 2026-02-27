fa1707ad7d37ed2f538afc6caffa07f1ce00e692
obolentsevavika14-cmd <obolentsevavika14@gmail.com>
2026-02-27 17:31:43 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,4 @@
+# Настройки бота
+BOT_TOKEN = "8654478208:AAFGaU_cli-wuLHrPzkVskNSYznBwjpji7A"  # Твой токен
+MORNING_TIME = "07:00"  # время утреннего уведомления
+EVENING_TIME = "20:00"  # время вечернего отчёта
